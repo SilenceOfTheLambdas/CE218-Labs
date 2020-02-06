1820a6351bf6d6a96980cd4c83e601fb228a298c
Callum-James Smith <techdragon@tuta.io>
2020-02-06 15:13:29 +0000

    mostly finished lab2

---
+++ src/utilities/Vector2D.java
@@ -0,0 +1,167 @@
+package utilities;
+
+// mutable 2D vectors
+public final class Vector2D {
+    public double x, y;
+
+    // constructor for zero vector
+    public Vector2D() {
+        x = 0;
+        y = 0;
+    }
+
+    // constructor for vector with given coordinates
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // constructor that copies the argument vector
+    public Vector2D(Vector2D v) {
+        set(v);
+    }
+
+    // set coordinates
+    public Vector2D set(double x, double y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    // set coordinates based on argument vector
+    public Vector2D set(Vector2D v) {
+        x = v.x;
+        y = v.y;
+        return this;
+    }
+
+    // compare for equality (note Object type argument)
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+
+        Vector2D other = (Vector2D)o;
+        return Math.hypot(x, y) == Math.hypot(other.x, other.y);
+    }
+
+    // String for displaying vector as text
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    //  magnitude (= "length") of this vector
+    public double mag() {
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
+    // angle between vector and horizontal axis in radians in range [-PI,PI]
+// can be calculated using Math.atan2
+    public double angle() {
+        double angle = Math.atan2(y, x) * Math.toRadians(angle());
+        if (angle < 0) angle += 360;
+        return angle;
+    }
+
+    // angle between this vector and another vector in range [-PI,PI]
+    public double angle(Vector2D other) {
+        double angle = 0;
+        if (other.x > x || other.y > y) angle = Math.atan2(other.x, other.y) / Math.atan2(x, y);
+        if (x > other.x || y > other.y) angle = Math.atan2(x, y) / Math.atan2(other.x, other.y);
+        if (angle < 0) angle += 360;
+        return angle;
+    }
+
+    // add argument vector
+    public Vector2D add(Vector2D v) {
+        x += v.x;
+        y += v.y;
+        return this;
+    }
+
+    // add values to coordinates
+    public Vector2D add(double x, double y) {
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+
+    // weighted add - surprisingly useful
+    public Vector2D addScaled(Vector2D v, double fac) {
+        this.x += v.x * fac;
+        this.y += v.y * fac;
+        return this;
+    }
+
+    // subtract argument vector
+    public Vector2D subtract(Vector2D v) {
+        x -= v.x;
+        y -= v.y;
+        return this;
+    }
+
+    // subtract values from coordinates
+    public Vector2D subtract(double x, double y) {
+        this.x -= x;
+        this.y -= y;
+        return this;
+    }
+
+    // multiply with factor
+    public Vector2D mult(double fac) {
+        this.x *= fac;
+        this.y *= fac;
+        return this;
+    }
+
+    // rotate by angle given in radians
+    public Vector2D rotate(double angle) {
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+
+        double newX = this.x * cos - this.y * sin;
+        double newY = this.x * sin + this.y * cos;
+
+        this.x = newX;
+        this.y = newY;
+
+        return this;
+    }
+
+    // "dot product" ("scalar product") with argument vector
+    public double dot(Vector2D v) {
+        return x * v.x + y * v.y;
+    }
+
+    // distance to argument vector
+    public double dist(Vector2D v) {
+        final double x_d = v.x - x;
+        final double y_d = v.y - y;
+        return x_d * x_d + y_d * y_d;
+    }
+
+    // normalise vector so that magnitude becomes 1
+    public Vector2D normalise() {
+        double len = mag();
+        if (len != 0) {
+            x /= len;
+            y /= len;
+        }
+        return this;
+    }
+
+    // wrap-around operation, assumes w> 0 and h>0
+// remember to manage negative values of the coordinates
+    public Vector2D wrap(double w, double h) {
+
+    }
+
+    // construct vector with given polar coordinates
+    public static Vector2D polar(double angle, double mag) {
+        Vector2D v = new Vector2D();
+        v.rotate(angle);
+        v.mult(mag);
+
+        return v;
+    }
+}
